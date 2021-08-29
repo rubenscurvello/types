ad3cce84ffd0b272a678b25623dc5fdc9adfc485
Rubens Curvello <rubens.curvello@rhimto.com.br>
2021-08-29 17:06:07 -0300

    export ftp

--- index.ts
+++ index.ts
@@ -1,8 +1,17 @@
-import  ConfigAuthAPI  from "./types/config.auth.api.type";
-import  ConfigAuthFTP  from "./types/config.auth.ftp.type";
-import  ConfigAuth  from "./types/config.auth.type";
-import  ConfigFTP  from "./types/config.ftp.type";
-import  ConfigHTTP  from "./types/config.http.type";
-import  Config  from "./types/config.type";
+import ConfigAuthAPI from "./types/config.auth.api.type";
+import ConfigAuthFTP from "./types/config.auth.ftp.type";
+import ConfigAuth from "./types/config.auth.type";
+import ConfigFTP from "./types/config.ftp.type";
+import ConfigHTTP from "./types/config.http.type";
+import Config from "./types/config.type";
+import FTPInterface from "./interfaces/ftp.interface";
 
-export { ConfigAuthAPI, ConfigAuthFTP, ConfigAuth, ConfigFTP, ConfigHTTP, Config};
+export {
+  ConfigAuthAPI,
+  ConfigAuthFTP,
+  ConfigAuth,
+  ConfigFTP,
+  ConfigHTTP,
+  Config,
+  FTPInterface,
+};
