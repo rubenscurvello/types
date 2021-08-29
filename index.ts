772225892a7f85823b09c349fa291d50acbad59d
Rubens Curvello <rubens.curvello@rhimto.com.br>
2021-08-29 16:40:46 -0300

    exports

---
+++ index.ts
@@ -0,0 +1,8 @@
+import  ConfigAuthAPI  from "./types/config.auth.api.type";
+import  ConfigAuthFTP  from "./types/config.auth.ftp.type";
+import  ConfigAuth  from "./types/config.auth.type";
+import  ConfigFTP  from "./types/config.ftp.type";
+import  ConfigHTTP  from "./types/config.http.type";
+import  Config  from "./types/config.type";
+
+export { ConfigAuthAPI, ConfigAuthFTP, ConfigAuth, ConfigFTP, ConfigHTTP, Config};
